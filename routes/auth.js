92bfed20148567f13c4c5bafddb914af175a524b
Kazmi4530181 <kazmi4530181@cloud.neduet.edu.pk>
2025-05-29 17:42:24 +0500

    update 29/05/25

--- routes/auth.js
+++ routes/auth.js
@@ -243,7 +243,7 @@ const handleSocialLogin = (profile, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://api.carsfinderpro.com/auth/google/callback'
+    callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
     handleSocialLogin(profile, done);
 }));
